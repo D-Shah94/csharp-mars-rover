933081d7419334eae491cd6252ef383025ea47d2
D-Shah94 <dharam_shah@hotmail.co.uk>
2026-04-13 09:54:25 +0100

    Package updates and dummy test added to ensure project has been correctly configured

---
+++ MarsRover.Tests/HelloWorldTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover.Tests
+{
+    public class HelloWorldTests
+    {
+        [Test]
+        public void Successfully_runs_a_test()
+        {
+            Assert.That(true, Is.True);
+        }
+    }
+}
