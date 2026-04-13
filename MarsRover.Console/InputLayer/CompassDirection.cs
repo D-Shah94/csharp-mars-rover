1da966f47a9fda52eefb73d60409c94a3fc72c44
D-Shah94 <dharam_shah@hotmail.co.uk>
2026-04-13 11:25:11 +0100

    Create directory for InputLayer with Instruction and CompassDirection enums

---
+++ MarsRover.Console/InputLayer/CompassDirection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover.Console.InputLayer
+{
+    enum CompassDirection
+    {
+        N,
+        S,
+        W,
+        E
+    }
+}
