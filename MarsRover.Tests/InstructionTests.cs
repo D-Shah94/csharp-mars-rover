99c1a55a6315f6da191ecdab03ff254091f5b11f
D-Shah94 <dharam_shah@hotmail.co.uk>
2026-04-13 15:36:20 +0100

    Create Tests for InstructionTests

--- MarsRover.Tests/InstructionTests.cs
+++ MarsRover.Tests/InstructionTests.cs
@@ -1,4 +1,6 @@
-﻿namespace MarsRover.Tests;
+﻿using MarsRover.Console.InputLayer;
+
+namespace MarsRover.Tests;
 
 public class InstructionTests
 {
@@ -7,9 +9,32 @@ public class InstructionTests
     {
     }
 
+    [TestCase(Instruction.L, 0)]
+    [TestCase(Instruction.R, 1)]
+    [TestCase(Instruction.M, 2)]
+
     [Test]
-    public void Test1()
+    public void Instruction_ShouldMapToSpecificIntegers(Instruction instruction, int expectedValue)
     {
-        Assert.Pass();
+        // Arrange & Act
+        int actualValue = (int)instruction;
+
+        // Assert
+        Assert.That(actualValue, Is.EqualTo(expectedValue));
+    }
+
+    [TestCase("L", Instruction.L)]
+    [TestCase("R", Instruction.R)]
+    [TestCase("M", Instruction.M)]
+
+
+    [Test]
+    public void Instruction_ShouldParseFromValidString(string input, Instruction expected)
+    {
+        // Act
+        var result = Enum.Parse<Instruction>(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 }
