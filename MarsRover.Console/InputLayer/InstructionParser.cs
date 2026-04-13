d179a6c64bf4da2143077cd9782694f9bb4ca63a
D-Shah94 <dharam_shah@hotmail.co.uk>
2026-04-13 14:12:46 +0100

    Create empty classes for parsers

---
+++ MarsRover.Console/InputLayer/InstructionParser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover.Console.InputLayer
+{
+    public class InstructionParser()
+    {
+
+
+    }
+}
+
+//int xDimension, int yDimension, int xStart, int yStart, CardinalDirection facingDir, Instruction rawInstruction
