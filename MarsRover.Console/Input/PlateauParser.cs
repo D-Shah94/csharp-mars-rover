93f6dd78d86704a1fff1e541af90bd7004736086
D-Shah94 <dharam_shah@hotmail.co.uk>
2026-04-14 11:33:34 +0100

    Create tests for PlateauParserTests and improved logic of the PlateauParser Class

--- MarsRover.Console/Input/PlateauParser.cs
+++ MarsRover.Console/Input/PlateauParser.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MarsRover.Console.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection.Metadata.Ecma335;
@@ -7,8 +8,27 @@ using System.Threading.Tasks;
 
 namespace MarsRover.Console.Input
 {
-    public class PlateauParser(string input)
+    public static class PlateauParser
     {
+        public static PlateauSize? Parse(string input)
+        {
+            if (String.IsNullOrWhiteSpace(input))
+            {
+                return null;
+            }
+
+            var parts = input.Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            if (parts.Length == 2 
+                && int.TryParse(parts[0], out int maxX) 
+                && int.TryParse(parts[1], out int maxY)
+                )
+            {
+                return (maxX >= 0 && maxY >= 0 ? new PlateauSize(maxX, maxY) : null);
+            }
+
+            return null;
+        }
 
 
     }
--- MarsRover.Tests/InputTests/PlateauParserTests.cs
+++ MarsRover.Tests/InputTests/PlateauParserTests.cs
@@ -4,8 +4,44 @@ namespace MarsRover.Tests.InputLayerTests;
 
 public class PlateauParserTests
 {
-    [SetUp]
-    public void Setup()
+    [TestCase("5 5", 5, 5)]
+    [TestCase("10 20", 10, 20)]
+    [TestCase("0 0", 0, 0)]
+    [Test]
+    public void Parse_ValidInput_ShouldReturnPlateauSize(string rawInput, int xExpectedValue, int yExpectedValue)
     {
+        // Arrange
+        
+        // Act
+        var parsedInput = PlateauParser.Parse(rawInput);
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(parsedInput, Is.Not.Null, "The parser returned null for valid input.");
+            Assert.That(parsedInput?.MaxX, Is.EqualTo(xExpectedValue), "MaxX was not parsed correctly.");
+            Assert.That(parsedInput?.MaxY, Is.EqualTo(yExpectedValue), "MaxX was not parsed correctly.");
+        });
     }
-}                                                                                     
+
+
+    [TestCase("5 5 5")]
+    [TestCase("5")]
+    [TestCase("A B")]
+    [TestCase("-1 5")]
+    [TestCase("")]
+    [TestCase(null)]
+    [Test]
+    public void Parse_InvalidInput_ShouldReturnNull(string? rawInput)
+    {
+        // Arrange
+
+        // Act
+        var parsedInput = PlateauParser.Parse(rawInput);
+
+        // Assert
+        Assert.That(parsedInput, Is.Null, $"The parser should return null for invalid input: '{rawInput}'");
+    }
+
+
+}
